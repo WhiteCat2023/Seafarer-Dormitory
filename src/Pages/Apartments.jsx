0196ea5a36ae51c99bf1a0a1a2cad2d9948134de
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2025-01-12 03:05:18 +0800

    tuned up the content viewer a bit added edit and delete buttons(not functioning yet)

--- src/Pages/Apartments.jsx
+++ src/Pages/Apartments.jsx
@@ -56,7 +56,7 @@ export default function Apartments(){
         }
         try{
             return items.length > 0 ? (items.map(item => (
-                <li className='flex justify-between items-center p-4 text-black border-b cursor-pointer' key={item.id} onClick={() => handleItemClick(item.id)}>
+                <li className='flex justify-between items-center p-4 text-black border-b cursor-pointer hover:bg-blue-50' key={item.id} onClick={() => handleItemClick(item.id)}>
                     <span className='flex-grow'>
                         <p className='font-semibold text-start text-gray-800'>{item.apartment_name}</p>
                         <span className='flex flex-col ps-2 text-start text-xs'>
@@ -80,34 +80,36 @@ export default function Apartments(){
     return(
         <>
             <div className="container mx-auto lg:mt-10 lg:px-8 " style={{height: 'calc(100% - 64px)'}}>
-                <div className="flex items-center justify-between mb-5 flex-col md:flex-row ">
+                <div className={`lg:flex items-center justify-between lg:mb-5 flex-col md:flex-row ${isListVisible ? 'flex' : 'hidden' }`}>
                     <nav className="flex items-center mb-4 md:mb-0 flex-grow md:flex-grow-0 w-full px-3 lg:px-0">
                         <h1 className="md:text-5xl font-outfit font-semibold text-3xl text-gray-600">Apartments</h1>
-                        <button onClick={openModal} className="bg-primary px-2 py-2 lg:px-3 lg:py-2 rounded-xl text-white ms-4 hover:bg-transparent hover:border-2 hover:border-blue-500 hover:text-blue-500 border-2 border-transparent flex justify-center text-sm lg:text-base">
+                        <button onClick={openModal} className="bg-primary px-2 py-2 lg:px-3 lg:py-2 rounded-xl text-white ms-4 hover:bg-transparent hover:border-2 hover:border-blue-500 hover:text-blue-500 border-2 border-transparent flex justify-center text-sm lg:text-base" style={{ display: isListVisible ? 'flex': 'none'}}>
                             <i className='lg:text-2xl text-xl me-1 lg:me-2 flex justify-center'>
                                 <BiClinic />
                             </i>
                             New Apartment</button>
                     </nav>
-                    <div className="relative w-full md:w-1/4 flex-grow md:flex-grow-0 px-2 lg:px-0">
+                    <div className="relative w-full md:w-1/4 flex-grow md:flex-grow-0 px-2 lg:px-0" style={{ display: isListVisible ? 'block': 'none'}}>
                         <input className="rounded-full w-full ps-10 border-blue-500 border-2" type="search" placeholder="Search"/>
                         <i className='absolute lg:left-3 left-5 top-3 -translate-y-1 text-2xl flex justify-center'><BiSearchAlt/></i>
                     </div>
                 </div>
-                <div className='lg:border-2 lg:h-3/4 h-5/6 lg:rounded-3xl lg:border-blue-500 relative overflow-auto'>
-                    <div className='border-b-2 border-blue-500 justify-between flex items-center px-4 pb-1 pt-3 sticky top-0 left-0 bg-white'>
-                        <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='loader'></box-icon></i>
-                        <div className='flex items-center'>
-                            <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon type='solid' name='chevron-left'></box-icon></i>
-                            <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='chevrons-left' ></box-icon></i>
-                            <p className='mx-2'>1</p>
-                            <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='chevrons-right' ></box-icon></i>
-                            <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='chevron-right' type='solid' ></box-icon></i>
+                <div className='lg:border-2 lg:h-3/4 h-full lg:rounded-3xl lg:border-blue-500 relative overflow-auto'>
+                    <div style={{ display: isListVisible ? 'block': 'none', height: 'calc(100% - 50px)'}}>
+                        <div className='border-b-2 border-blue-500 justify-between flex items-center px-4 pb-1 pt-3 sticky top-0 left-0 bg-white'>
+                            <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='loader'></box-icon></i>
+                            <div className='flex items-center'>
+                                <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon type='solid' name='chevron-left'></box-icon></i>
+                                <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='chevrons-left' ></box-icon></i>
+                                <p className='mx-2'>1</p>
+                                <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='chevrons-right' ></box-icon></i>
+                                <i className='p-1 rounded-full hover:bg-blue-100 cursor-pointer flex justify-center'><box-icon name='chevron-right' type='solid' ></box-icon></i>
+                            </div>
                         </div>
+                        <ul className=' w-full' id='apartment-li' ref={apartmentList}>             
+                            {displayList()}
+                        </ul>
                     </div>
-                    <ul className=' w-full' id='apartment-li' ref={apartmentList} style={{ display: isListVisible ? 'block': 'none', height: 'calc(100% - 50px)'}}>             
-                        {displayList()}
-                    </ul>
                     {isItemClicked && <ContentViewer isOpen={isItemClicked} onClose={handleBackBtnClick} item={selectedItem}/>}
                 </div>
             </div>
--- src/Pages/components/ContentViewer.jsx
+++ src/Pages/components/ContentViewer.jsx
@@ -1,20 +1,29 @@
-import { BiArrowBack } from "react-icons/bi";
+import { BiArrowBack, BiMap, BiEdit, BiTrash } from "react-icons/bi";
 
 export default function ContentViewer({isOpen, onClose, item}){
     return(
         <>
-            <div className='w-full text-start' style={{display: isOpen ? 'block': 'none', height: 'calc(100% - 50px)' }} id={'item-id-' + item.id}>
-                <div className='p-4'><i onClick={onClose}><BiArrowBack className='text-2xl'/></i></div>
-                <div className="lg:px-8 pb-8 flex overflow-hidden flex-col-reverse lg:flex-row" style={{height:'calc(100% - 56px)'}}>
-                    <div className="lg:w-2/4 overflow-y-auto">
-                        <p className="font-semibold text-gray-700 text-2xl">{item.apartment_name}</p>
+            <div className='w-full text-start h-full text-gray-600' style={{display: isOpen ? 'block': 'none'}} id={'item-id-' + item.id}>
+                <div className='p-4 pb-1 flex justify-between text-2xl'>
+                    <i onClick={onClose} className="rounded-full hover:bg-blue-100 p-1"><BiArrowBack/></i>
+                    <div className="flex w-24 justify-evenly">
+                        <i className="rounded-full hover:bg-blue-100 p-1"><BiEdit/></i>
+                        <i className="rounded-full hover:bg-blue-100 p-1"><BiTrash/></i>
+                    </div>
+                </div>
+                <div className="lg:ps-8 pb-8 flex overflow-auto lg:overflow-hidden flex-col items-center lg:flex-row" style={{height:'calc(100% - 56px)'}}>
+                    <div className="lg:w-2/4 h-full flex justify-center p-4">
+                        <img className="h-full rounded-2xl" src="https://www.tollbrothersapartmentliving.com/wp-content/uploads/2023/03/005_Toll_4_11_19_UnionPl-983x720-1.jpg"/>
+                    </div>
+                    <div className="lg:w-2/4 lg:overflow-y-auto h-full mx-4 lg:me-0 pt-4 lg:px-8 ">
+                        <span className="flex items-center">
+                            <p className="font-semibold text-gray-700 text-2xl">{item.apartment_name}<span className="text-sm text-gray-400"> ({item.isAvailable == 1? 'Available' : 'Unavailable'})</span></p>
+                        </span>
                         <p className="text-gray-500"> Number of rooms: <span className="text-black">{item.number_of_rooms}</span></p>
-                        <p className="">{item.location}</p>
-                        <p className="lg:mt-10 me-8 text-justify ">{item.description}</p>
+                        <p className="flex"><BiMap/>{item.location}</p>
+                        <p className="mt-10 text-sm md:text-base text-justify ">{item.description}</p>
                     </div>
-                    <div className="lg:w-2/4 h-full flex justify-center">
-                        <img className="h-full" src="https://www.tollbrothersapartmentliving.com/wp-content/uploads/2023/03/005_Toll_4_11_19_UnionPl-983x720-1.jpg"/>
-                    </div>
+                    
                 </div>
             </div>
         </>
