fb9bd76855d0b408c51f718ba37ce1ecd6db5f43
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2025-02-11 16:01:25 +0800

    UPDATED 02-11-2025

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -5,8 +5,8 @@ export default function Card({ src, name, availability, price, onClick}){
     const newSrc = Object.values(src);
     return(
         <>
-            <div className="w-2/4 sm:w-1/4  sm:border sm:rounded-3xl overflow-hidden sm:shadow-xl cursor-pointer sm:pb-4 p-1 sm:p-0"  >
-                <div className='w-full h-3/4'>
+            <div className="w-full h-full sm:border sm:rounded-xl overflow-hidden sm:shadow-xl cursor-pointer "  >
+                <div className='h-60'>
                     <Carousel items={newSrc}/>
                 </div>
                 <div className="text-start w-full p-4 pt-2 text-gray-400" key={name} onClick={onClick}>
